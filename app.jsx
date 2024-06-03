c6fbd54413429701eb0ddf25abdfff4cbbb8462d
Danielroxs <danroxsll@gmail.com>
2024-06-03 16:38:05 -0600

    Renderizado de listas

--- app.jsx
+++ app.jsx
@@ -1,4 +1,4 @@
-function Item({ name, isPacked }) {
+/* function Item({ name, isPacked }) {
   return (
     <li className="item">
       {name} {isPacked ? "✔" : "❌"}
@@ -16,5 +16,49 @@ export default function PackingList() {
         <Item isPacked={false} name="Fotografía de Tam" />
       </ul>
     </section>
+  );
+}
+ */
+
+// Renderizado de Listas
+
+import { people } from "./data.js";
+import { getImageUrl } from "./utils.js";
+
+export default function List() {
+  const chemists = people.filter((person) => person.profession === "químico");
+  const everyoneElse = people.filter(
+    (person) => person.profession !== "químico"
+  );
+  return (
+    <article>
+      <h1>Científicos</h1>
+      <h2>Químicos</h2>
+      <ul>
+        {chemists.map((person) => (
+          <li key={person.id}>
+            <img src={getImageUrl(person)} alt={person.name} />
+            <p>
+              <b>{person.name}:</b>
+              {" " + person.profession + " "}
+              conocido/a por {person.accomplishment}
+            </p>
+          </li>
+        ))}
+      </ul>
+      <h2>Todos los demás</h2>
+      <ul>
+        {everyoneElse.map((person) => (
+          <li key={person.id}>
+            <img src={getImageUrl(person)} alt={person.name} />
+            <p>
+              <b>{person.name}:</b>
+              {" " + person.profession + " "}
+              conocido/a por {person.accomplishment}
+            </p>
+          </li>
+        ))}
+      </ul>
+    </article>
   );
 }
