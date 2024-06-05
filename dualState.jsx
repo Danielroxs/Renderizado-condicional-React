2d30bc42467b721e229f5bf52b0509888b003fe8
Danielroxs <danroxsll@gmail.com>
2024-06-05 17:49:58 -0600

    handle dual state

---
+++ dualState.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { sculptureList } from "./data.js";
+
+export default function Gallery() {
+  const [index, setIndex] = useState(0);
+  const [showMore, setShowMore] = useState(false);
+
+  let hasPrev = index > 0;
+  let hasNext = index < sculptureList.length - 1;
+
+  function handlePrevClick() {
+    if (hasPrev) {
+      setIndex(index - 1);
+    }
+  }
+
+  function handleNextClick() {
+    if (hasNext) {
+      setIndex(index + 1);
+    }
+  }
+
+  function handleMoreClick() {
+    setShowMore(!showMore);
+  }
+
+  let sculpture = sculptureList[index];
+  return (
+    <>
+      <button onClick={handlePrevClick} disabled={!hasPrev}>
+        Anterior
+      </button>
+      <button onClick={handleNextClick} disabled={!hasNext}>
+        Siguiente
+      </button>
+      <h2>
+        <i>{sculpture.name} </i>
+        por {sculpture.artist}
+      </h2>
+      <h3>
+        ({index + 1} de {sculptureList.length})
+      </h3>
+      <button onClick={handleMoreClick}>
+        {showMore ? "Ocultar" : "Mostrar"} detalles
+      </button>
+      {showMore && <p>{sculpture.description}</p>}
+      <img src={sculpture.url} alt={sculpture.alt} />
+    </>
+  );
+}
