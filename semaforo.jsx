46f2fa5c75fba92aaade317fba49b10cc3679f6d
Danielroxs <danroxsll@gmail.com>
2024-06-06 19:35:49 -0600

    Estate Semaforo

---
+++ semaforo.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export default function TrafficLight() {
+  const [walk, setWalk] = useState(true);
+
+  function handleClick() {
+    setWalk(!walk);
+    alert(walk ? "Parar es lo siguiente" : "Caminar es lo siguiente");
+  }
+
+  return (
+    <>
+      <button onClick={handleClick}>
+        Cambia a {walk ? "Parar" : "Caminar"}
+      </button>
+      <h1
+        style={{
+          color: walk ? "darkgreen" : "darkred",
+        }}
+      >
+        {walk ? "Caminar" : "Parar"}
+      </h1>
+    </>
+  );
+}
