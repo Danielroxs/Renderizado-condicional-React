074ed2667fc1ebb03c2d22221c60ef2c328243ed
Danielroxs <danroxsll@gmail.com>
2024-06-11 16:48:50 -0600

    rendering using state

--- StateForm.jsx
+++ StateForm.jsx
@@ -49,7 +49,7 @@ export default function Form() {
   );
 }
  */
-
+/* 
 import { useState } from "react";
 
 export default function Form() {
@@ -98,6 +98,134 @@ export default function Form() {
       <p>
         {person.firstName} {person.lastName} ({person.email})
       </p>
+    </>
+  );
+} */
+/* 
+import { useState } from "react";
+
+export default function Form() {
+  const [person, setPerson] = useState({
+    name: "Niki de Saint Phalle",
+    artwork: {
+      title: "Nana azul",
+      city: "Hamburgo",
+      image: "https://i.imgur.com/Sd1AgUOm.jpg",
+    },
+  });
+
+  function handleNameChange(e) {
+    setPerson({
+      ...person,
+      name: e.target.value,
+    });
+  }
+
+  function handleTitleChange(e) {
+    setPerson({
+      ...person,
+      artwork: {
+        ...person.artwork,
+        title: e.target.value,
+      },
+    });
+  }
+
+  function handleCityChange(e) {
+    setPerson({
+      ...person,
+      artwork: {
+        ...person.artwork,
+        city: e.target.value,
+      },
+    });
+  }
+
+  function handleImageChange(e) {
+    setPerson({
+      ...person,
+      artwork: {
+        ...person.artwork,
+        image: e.target.value,
+      },
+    });
+  }
+
+  return (
+    <>
+      <label>
+        Nombre:
+        <input value={person.name} onChange={handleNameChange} />
+      </label>
+      <label>
+        Título:
+        <input value={person.artwork.title} onChange={handleTitleChange} />
+      </label>
+      <label>
+        Ciudad:
+        <input value={person.artwork.city} onChange={handleCityChange} />
+      </label>
+      <label>
+        Imagen:
+        <input value={person.artwork.image} onChange={handleImageChange} />
+      </label>
+      <p>
+        <i>{person.artwork.title}</i>
+        {" por "}
+        {person.name}
+        <br />
+        (situada en {person.artwork.city})
+      </p>
+      <img src={person.artwork.image} alt={person.artwork.title} />
+    </>
+  );
+}
+ */
+
+import { useState } from "react";
+
+export default function Scoreboard() {
+  const [player, setPlayer] = useState({
+    firstName: "Ranjani",
+    lastName: "Shettar",
+    score: 10,
+  });
+
+  function handlePlusClick() {
+    setPlayer({
+      ...player,
+      score: player.score + 1,
+    });
+  }
+
+  function handleFirstNameChange(e) {
+    setPlayer({
+      ...player,
+      firstName: e.target.value,
+    });
+  }
+
+  function handleLastNameChange(e) {
+    setPlayer({
+      ...player,
+      lastName: e.target.value,
+    });
+  }
+
+  return (
+    <>
+      <label>
+        Score: <b>{player.score}</b>{" "}
+        <button onClick={handlePlusClick}>+1</button>
+      </label>
+      <label>
+        Nombre:
+        <input value={player.firstName} onChange={handleFirstNameChange} />
+      </label>
+      <label>
+        Apellido:
+        <input value={player.lastName} onChange={handleLastNameChange} />
+      </label>
     </>
   );
 }
