c70464f22f8f47c0c4c3f2f0b819982d14c21760
Danielroxs <danroxsll@gmail.com>
2024-06-11 19:21:42 -0600

    using slice with state in array

---
+++ SliceUsingState.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+let nextId = 3;
+const initialArtists = [
+  { id: 0, name: "Marta Colvin Andrade" },
+  { id: 1, name: "Lamidi Olonade Fakeye" },
+  { id: 2, name: "Louise Nevelson" },
+];
+
+export default function List() {
+  const [name, setName] = useState("");
+  const [artists, setArtists] = useState(initialArtists);
+
+  function handleClick() {
+    const insertAt = 1; // Podría ser cualquier índice
+    const nextArtists = [
+      // Elementos antes del punto de inserción:
+      ...artists.slice(0, insertAt),
+      // Nuevo ítem:
+      { id: nextId++, name: name },
+      // Elementos después del punto de inserción:
+      ...artists.slice(insertAt),
+    ];
+    setArtists(nextArtists);
+    setName("");
+  }
+
+  return (
+    <>
+      <h1>Escultores inspiradores:</h1>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button onClick={handleClick}>Insertar</button>
+      <ul>
+        {artists.map((artist) => (
+          <li key={artist.id}>{artist.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
